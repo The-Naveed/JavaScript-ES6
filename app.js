7538344b71abc4c52dd5dc38a814374898616d01
Your Name <you@example.com>
2024-07-16 11:32:28 +0500

    Arrow Functions Completed

---
+++ app.js
@@ -0,0 +1,57 @@
+// // let Keyword
+
+// let firstName = "Naveed";
+
+// firstName = "Ahmed";
+
+// console.log(firstName);
+
+// if (true) {
+
+//     firstName = "Naveed Ahmed";
+
+//     console.log(firstName);
+
+// }
+
+// XXX
+
+// const Keyword
+
+// const firstName = "Naveed";
+
+// firstName = "Ahmed";
+
+// Its'nt allowed to reinitialization
+
+// XXX
+
+// Arrow Functions
+
+// const foo = () => { return (10) };
+
+// console.log(foo())
+
+// or
+
+// const foo = () => 10;
+
+// console.log(foo())
+
+// or
+
+// const foo = _ => 10;
+
+// console.log(foo())
+
+// or
+
+// const foo = (value) => { console.log(value) };
+
+// foo(10)
+
+// or
+
+const foo = value => console.log(value);
+
+foo(10)
